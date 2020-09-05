9e00a07e4edf75bfbd8001dd414527cfaabda903
Arifulhaque-02128 <arifulhaque02128@gmail.com>
2020-09-06 01:22:01 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect, createContext } from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Header from './Component/Header/Header';
+import Home from './Component/Home/Home';
+import PostDetails from './Component/PostDetails/PostDetails';
+import NotFound from './Component/NotFound/NotFound';
+
+export const CategoryContext = createContext();
 
 function App() {
+  const [post, setPost] = useState([]);
+  useEffect( () => {
+    const url = "https://jsonplaceholder.typicode.com/posts";
+    fetch(url)
+    .then(res => res.json())
+    .then(data => setPost(data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CategoryContext.Provider value={post}>
+      <Header></Header>
+      <Router>
+        <Switch>
+          <Route path="/home">
+            <Home posts={post}></Home>
+          </Route>
+          <Route exact path="/">
+            <Home posts={post}></Home>
+          </Route>
+          <Route path="/posts/:postId">
+            <PostDetails posts={post}></PostDetails>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
+    </CategoryContext.Provider>
   );
 }
 
